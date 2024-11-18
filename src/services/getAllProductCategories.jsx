5071cb242465502d618817655227acc904dfcda8
JihanSalsabilah14 <jihansalsabilah.22013@mhs.unesa.ac.id>
2024-11-18 08:17:17 +0700

    belom-selesai

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,16 @@
+const getAllProductCategories = () => {
+  return [
+    {
+      id: "BS",
+      slug: "badut-sulap",
+      name: `Badut Sulap`,
+    },
+    {
+      id: "BA",
+      slug: "badut-atraksi",
+      name: `Badut Atraksi`,
+    },
+  ];
+};
+
+export default getAllProductCategories;
